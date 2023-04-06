e51284e9e8a942bc69bc019e5c5ffddf642f0479
aprilkrg <aprilkrgonzales@gmail.com>
2023-04-06 15:40:44 -0600

    add view for categories show

--- controllers/categories.js
+++ controllers/categories.js
@@ -17,4 +17,21 @@ router.get("/", async (req, res) => {
     }
 })
 
+// http GET url /categories/:id - show one category
+router.get("/:id", async (req, res) => {
+    try {
+        const oneCategory = await db.category.findOne({
+            where: {
+                id: req.params.id
+            }, 
+            include: [db.project]
+        })
+        res.render("categories/show.ejs", {
+            category: oneCategory
+        })
+    } catch(err) {
+        res.status(400).render("main/404")
+    }
+})
+
 module.exports = router
